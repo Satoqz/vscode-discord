ce54f049acd8ae298f94d130a3938bd727b800ff
Satoqz <satoqzzz@gmail.com>
2020-07-15 19:06:50 +0200

    gg

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,7 @@
 Added first icons (ts & js)
 
 Added icons to show (in-)activity
+
+### 0.0.8
+
+Made it sooooo much better omg
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,25 @@
 //this is horrible, i made it for fun only, okay ????? OKAY !!!!!
-import * as vscode from "vscode"
-import { workspace, TextDocumentChangeEvent, TextDocument, window, debug } from "vscode";
+import { workspace, TextDocumentChangeEvent, window, debug, ExtensionContext, StatusBarAlignment, StatusBarItem, TextEditor } from "vscode";
 import { Client, register, Presence } from "discord-rpc";
 import { imageKeys } from "./imageKeys";
 
-const rpcData: Presence = {
-	details: "Just launched",
-	state: "No file edited yet",
-	smallImageKey: "active",
-	smallImageText: "Active in VSCode",
-    largeImageKey: "vscode",
-	largeImageText: "What will he code?!? 😳",
-	instance: true
-};
-
-let startTimestamp: Date;
+const rpcData: Presence = {};
 
 const clientId = "732565262704050298";
 
 const rpc = new Client({ transport: "ipc" });
 
-export function activate() {
+let statusBar: StatusBarItem;
+
+let activeTimeout: NodeJS.Timeout;
+
+export function activate(context: ExtensionContext) {
+
+	statusBar = window.createStatusBarItem(StatusBarAlignment.Left, 100);
+	context.subscriptions.push(statusBar);
+	statusBar.text = "Connecting to Discord...";
+	statusBar.show();
+	statusBar.tooltip
 
 	activateRPC();
 
@@ -35,16 +34,25 @@ function activateRPC() {
 
 	// once connected, start sending rich presence requests and listening to the vscode api
 	rpc.on("ready", () => {
+		
+		statusBar.text = "Connected to Discord";
 
 		// set "vscode just launched" presence
-		startTimestamp = new Date();
+		rpcData.details = "No file opened";
+		rpcData.state = workspace.name ? `in ${workspace.name}` : "No workspace 😳";
+		rpcData.smallImageKey = "active";
+		rpcData.smallImageText = "Active in VSCode";
+    	rpcData.largeImageKey = "vscode";
+		rpcData.largeImageText = "What will they code?!? 😳";
+		rpcData.instance = true;
 		setRPC();
 
 		// refresh presence every minute to catch unfocusing the window
-		setInterval(() => {
+		activeTimeout = setInterval(() => {
 
-			rpcData.smallImageText = `${window.state.focused ? "Active" : "Inactive"} in VSCode`;
-			rpcData.smallImageKey = window.state.focused ? "active" : "inactive";
+			if(window.state.focused) setActive(true);
+			else setActive(false);
+
 			setRPC();
 
 		}, 1000*60);
@@ -57,44 +65,68 @@ function registerVSCodeEvents() {
 
 	workspace.onDidChangeTextDocument((e: TextDocumentChangeEvent) => {
 
-	const fileName = resolveFileName(e.document.fileName);
+		// determine activity verb
+		const activity: string = debug.activeDebugSession ? "Debugging" : "Editing";
+		
+		rpcData.details = `${activity} ${resolveFileName(e.document.fileName)}`;
 
-	// determine activity verb
-	let activity: string;
-	if(debug.activeDebugSession) activity = "Debugging";
-	else activity = "Editing";
+		// if there are unsaved changes, add it behind the comma
+		rpcData.largeImageText = `${e.document.languageId} file, on line ${window.activeTextEditor.selection.active.line + 1}/${e.document.lineCount}${e.document.isDirty ? ", unsaved changes" : ""}`;
+		
+		setActive(true);
 
-	let fullActivity: string; 
+		setRPC();
+	});
 
-	if(fileName == "input") fullActivity = "managing source control";
+	window.onDidChangeActiveTextEditor((e: TextEditor) => {
+
+		// determine activity verb
+		// file has only been opened, so just "viewing"
+		const activity: string = debug.activeDebugSession ? "Debugging" : "Viewing";
 	
-	else fullActivity = `${activity} ${fileName}`;
+		rpcData.details = `${activity} ${resolveFileName(e.document.fileName)}`;
 
-	const image = imageKeys.find(i => i.matches.includes(e.document.languageId));
+		// new file, new timer
+		rpcData.startTimestamp = new Date();
 
-	// fallback to standard icon if no language-specific image was found
-	rpcData.largeImageKey = image ? image.key : "vscode";
+		// catch missing workspace
+		rpcData.state = workspace.name ? `in ${workspace.name}` : "No workspace 😳";
 
-	// catch missing workspace
-	rpcData.state = workspace.name ? `in ${workspace.name}` : "No workspace 😳";
-	
-	// if there are unsaved changes, add it behind the comma
-	rpcData.largeImageText = `${e.document.languageId} file, 
-	on line ${window.activeTextEditor.selection.active.line + 1}/${e.document.lineCount}
-	${e.document.isDirty ? ", unsaved changes" : ""}`;
+		const image = imageKeys.find(i => i.matches.includes(e.document.languageId));
+		// fallback to standard icon if no language-specific image was found
+		rpcData.largeImageKey = image ? image.key : "vscode";
 
-	rpcData.smallImageKey = "active";
-	rpcData.smallImageText = "Active in VSCode";
-	rpcData.startTimestamp = startTimestamp;
-	rpcData.details = fullActivity;
+		rpcData.largeImageText = `${e.document.languageId} file, ${e.document.lineCount} line${e.document.lineCount == 1 ? "" : "s"}`;
 
-	setRPC();
+		setActive(true);
 
+		setRPC();
+	});
+
+	//catch debug session start and stop immediately
+	debug.onDidStartDebugSession(() => {
+		setActive(true);
+		// remove activity verb and put "Debugging"
+		rpcData.details = `Debugging ${rpcData.details.split(" ").slice(1, Infinity).join(" ")}`;
+		setRPC();
+	});
+
+	debug.onDidTerminateDebugSession(() => {
+		// same thing in reverse
+		rpcData.details = `Viewing ${rpcData.details.split(" ").slice(1, Infinity).join(" ")}`;
+		setRPC();
 	});
 }
 
 function setRPC() {
 	if(true) rpc.setActivity(rpcData);
+}
+
+function setActive(active: boolean) {
+	//refresh activity status, will require calling setRPC() afterwards
+	activeTimeout.refresh();
+	rpcData.smallImageKey = active ? "active" : "inactive";
+	rpcData.smallImageText = `${active ? "Active" : "Inactive"} in VSCode`;
 }
 
 // will remove the path and leave the actual filename
