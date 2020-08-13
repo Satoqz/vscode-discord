cfc4d65787e6a9e3531f1bec2b5d72f0af5aa421
Satoqz <satoqzzz@gmail.com>
2020-08-13 15:51:35 +0200

    bruh

--- src/extension.ts
+++ src/extension.ts
@@ -15,7 +15,8 @@ let activeTimeout: NodeJS.Timeout;
 
 let errorCount: number = 0;
 
-export function activate(context: ExtensionContext) {
+export function activate(context: ExtensionContext)
+{
 
 	statusBar = window.createStatusBarItem(StatusBarAlignment.Left, 100);
 	context.subscriptions.push(statusBar);
@@ -27,15 +28,17 @@ export function activate(context: ExtensionContext) {
 
 }
 
-function activateRPC() {
+function activateRPC()
+{
 
 	// connect to discord
 	register(clientId);
 	rpc.login({ clientId }).catch(console.error);
 
 	// once connected, start sending rich presence requests and listening to the vscode api
-	rpc.on("ready", () => {
-		
+	rpc.on("ready", () =>
+	{
+
 		statusBar.text = "$(vm-active)";
 		statusBar.tooltip = "Connected to Discord";
 
@@ -50,9 +53,10 @@ function activateRPC() {
 		setRPC();
 
 		// refresh presence every minute to catch unfocusing the window
-		activeTimeout = setInterval(() => {
+		activeTimeout = setInterval(() =>
+		{
 
-			if(window.state.focused) setActive(true);
+			if (window.state.focused) setActive(true);
 			else setActive(false);
 
 			setRPC();
@@ -63,20 +67,24 @@ function activateRPC() {
 	});
 }
 
-function registerVSCodeEvents() {
+function registerVSCodeEvents()
+{
 
-	workspace.onDidChangeTextDocument((e: TextDocumentChangeEvent) => {
+	workspace.onDidChangeTextDocument((e: TextDocumentChangeEvent) =>
+	{
 
-		if(e.document.fileName.endsWith(".git")) return;
+		if (e.document.fileName.endsWith(".git")) return;
 
-		if(e.document.languageId == "scminput") {
+		if (e.document.languageId == "scminput")
+		{
 			rpcData.largeImageKey = "git";
 			rpcData.details = "Writing a commit message";
 		}
-		else {
+		else
+		{
 			// determine activity verb
 			const activity: string = debug.activeDebugSession ? "Debugging" : "Editing";
-		
+
 			rpcData.details = `${activity} ${resolveFileName(e.document.fileName)} | ${errorCount} problem${errorCount == 1?"":"s"} found`;
 
 			//current line count sometimes lacks behind when removing a lot of lines, wo we'll sync it
@@ -87,20 +95,22 @@ function registerVSCodeEvents() {
 			// if there are unsaved changes, add it behind the comma
 			rpcData.largeImageText = `${e.document.languageId} file, on line ${currentLine}/${e.document.lineCount}${e.document.isDirty ? ", unsaved changes" : ""}`;
 		}
-		
-		
+
+
 		setActive(true);
 
 		setRPC();
 	});
 
-	window.onDidChangeActiveTextEditor((e: TextEditor) => {
+	window.onDidChangeActiveTextEditor((e: TextEditor) =>
+	{
 		// will be undefined if no TextEditor is open anymore (editor closed instead of switched)
-		if(e) {
+		if (e)
+		{
 			// determine activity verb
 			// file has only been opened, so just "viewing"
 			const activity: string = debug.activeDebugSession ? "Debugging" : "Viewing";
-		
+
 			rpcData.details = `${activity} ${resolveFileName(e.document.fileName)} | ${errorCount} problem${errorCount == 1?"":"s"} found`;
 
 			// catch missing workspace
@@ -117,15 +127,18 @@ function registerVSCodeEvents() {
 
 			setRPC();
 		}
-		else {
-			/*	
+		else
+		{
+			/*
 			when the file is only switched, this will still be triggered a short time before the actual event
 			To prevent an unnecessary call, let's log the old status, wait half a second and check whether it is still the same
 			Only continue if the status is still the same, indicating that the editor has actually been closed and not just changed
 			*/
 			const temp = rpcData.details;
-			setTimeout(() => {
-				if(temp == rpcData.details) {
+			setTimeout(() =>
+			{
+				if (temp == rpcData.details)
+				{
 					rpcData.details = "No file opened";
 
 					rpcData.largeImageKey = "vscode";
@@ -144,26 +157,32 @@ function registerVSCodeEvents() {
 	});
 
 	//catch debug session start and stop immediately
-	debug.onDidStartDebugSession(() => {
+	debug.onDidStartDebugSession(() =>
+	{
 		setActive(true);
 		// remove activity verb and put "Debugging"
 		rpcData.details = `Debugging ${rpcData.details.split(" ").slice(1, Infinity).join(" ")}`;
 		setRPC();
 	});
 
-	debug.onDidTerminateDebugSession(() => {
+	debug.onDidTerminateDebugSession(() =>
+	{
 		// same thing in reverse
 		rpcData.details = `Viewing ${rpcData.details.split(" ").slice(1, Infinity).join(" ")}`;
 		setRPC();
 	});
 
-	languages.onDidChangeDiagnostics(() => {
+	languages.onDidChangeDiagnostics(() =>
+	{
 		const diag = languages.getDiagnostics();
 		let counted: number = 0;
-		diag.forEach(i => {
-			if(i[1]) {
-				i[1].forEach(i => {
-					if(i.severity == DiagnosticSeverity.Warning || i.severity == DiagnosticSeverity.Error) counted++;
+		diag.forEach(i =>
+		{
+			if (i[1])
+			{
+				i[1].forEach(i =>
+				{
+					if (i.severity == DiagnosticSeverity.Warning || i.severity == DiagnosticSeverity.Error) counted++;
 				});
 			}
 		});
@@ -171,21 +190,24 @@ function registerVSCodeEvents() {
 	});
 }
 
-function setRPC() {
-	if(true) rpc.setActivity(rpcData);
+function setRPC()
+{
+	if (true) rpc.setActivity(rpcData);
 }
 
-function setActive(active: boolean) {
+function setActive(active: boolean)
+{
 	//refresh activity status, will require calling setRPC() afterwards
 	activeTimeout.refresh();
 	rpcData.smallImageKey = active ? "active" : "inactive";
 	rpcData.smallImageText = `${active ? "Active" : "Inactive"} in VSCode`;
 }
 
-function setImageByLang(document: TextDocument) {
+function setImageByLang(document: TextDocument)
+{
 	let image = imageKeys.find(i => i.matches.includes(resolveFileName(document.fileName)));
-	if(!image) image = imageKeys.find(i => i.matches.includes(resolveFileExtension(document.fileName)));
-	if(!image) image = imageKeys.find(i => i.matches.includes(document.languageId));
+	if (!image) image = imageKeys.find(i => i.matches.includes(resolveFileExtension(document.fileName)));
+	if (!image) image = imageKeys.find(i => i.matches.includes(document.languageId));
 	// fallback to standard file icon if no language-specific image was found
 	rpcData.largeImageKey = image ? image.key : "file";
 }
