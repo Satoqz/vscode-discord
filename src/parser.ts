180a9682ce3268b175f6e84e8cd301b6e889f58c
satoqz <satoqzzz@gmail.com>
2021-07-03 21:39:08 +0200

    add folder option

--- src/parser.ts
+++ src/parser.ts
@@ -1,23 +1,23 @@
 import { Presence } from "discord-rpc";
 import {
-	DiagnosticSeverity,
-	languages,
-	TextDocument,
-	TextDocumentChangeEvent,
-	TextEditor,
-	window,
-	workspace,
-	WorkspaceConfiguration,
-	env,
+  DiagnosticSeverity,
+  languages,
+  TextDocument,
+  TextDocumentChangeEvent,
+  TextEditor,
+  window,
+  workspace,
+  WorkspaceConfiguration,
+  env,
 } from "vscode";
 
 import type { Client } from "./client";
-import { resolveFileName, resolveFileExtension, resolveIcon } from "./util";
+import { PathInfo } from "./util";
 
 const enum activity {
-	debugging = "debuggingText",
-	editing = "editingText",
-	viewing = "viewingText",
+  debugging = "debuggingText",
+  editing = "editingText",
+  viewing = "viewingText",
 }
 
 const isInsiders = env.appName.toLowerCase().includes("insiders");
@@ -25,149 +25,146 @@ const isInsiders = env.appName.toLowerCase().includes("insiders");
 const vsicon = isInsiders ? "insiders" : "vscode";
 
 const placeholderKey = isInsiders
-	? "placeholderTextInsiders"
-	: "placeholderText";
+  ? "placeholderTextInsiders"
+  : "placeholderText";
 
 const enum icons {
-	other = "text",
-	idle = "inactive",
+  other = "text",
+  idle = "inactive",
 }
 
 export class Parser {
-	constructor(private client: Client) {
-		this.config = client.config;
-	}
+  constructor(private client: Client) {
+    this.config = client.config;
+  }
 
-	public makeInitial() {
-		if (this.config.get("showTime"))
-			this.presence.startTimestamp = Date.now();
+  public makeInitial() {
+    if (this.config.get("showTime")) this.presence.startTimestamp = Date.now();
 
-		const placeholder = this.config.get<string>(placeholderKey);
-		this.presence.largeImageKey = vsicon;
-		this.presence.largeImageText = placeholder;
-		this.presence.smallImageKey = vsicon;
-		this.presence.smallImageText = placeholder;
+    const placeholder = this.config.get<string>(placeholderKey);
+    this.presence.largeImageKey = vsicon;
+    this.presence.largeImageText = placeholder;
+    this.presence.smallImageKey = vsicon;
+    this.presence.smallImageText = placeholder;
 
-		if (
-			this.config.get("showWorkspace") &&
-			!this.config
-				.get<string[]>("hideWorkspaces")
-				.includes(workspace.name)
-		)
-			this.presence.state = workspace.name
-				? this.config
-						.get<string>("workspaceText")
-						.replace(/{workspace}/g, workspace.name)
-				: "No workspace";
+    if (
+      this.config.get("showWorkspace") &&
+      !this.config.get<string[]>("hideWorkspaces").includes(workspace.name)
+    )
+      this.presence.state = this.makeState({ initial: true });
 
-		this.update();
-	}
+    this.update();
+  }
 
-	private makeActivity(type: activity, path: string) {
-		if (this.debugging) type = activity.debugging;
-		const file = resolveFileName(path);
-		const language = resolveFileExtension(path);
-		const excludeFile = this.config
-			.get<string[]>("hideFiles")
-			.includes(file);
-		const activityString = this.config
-			.get<string>(type)
-			.replace(/{file}/g, excludeFile ? "a file" : file)
-			.replace(/{language}/g, language);
-		const problemString = this.config.get("showProblems")
-			? this.config
-					.get<string>("problemsText")
-					.replace(/{count}/g, this.problems.toString())
-			: "";
-		return `${activityString} ${problemString}`;
-	}
+  private makeDetails(type: activity, info: PathInfo): string {
+    if (this.debugging) type = activity.debugging;
+    const excludeFile = this.config
+      .get<string[]>("hideFiles")
+      .includes(info.fileName);
+    const activityString = this.config
+      .get<string>(type)
+      .replace(/{file}/g, excludeFile ? "a file" : info.fileName)
+      .replace(/{extension}/g, info.extension);
+    const problemString = this.config.get("showProblems")
+      ? this.config
+          .get<string>("problemsText")
+          .replace(/{count}/g, this.problems.toString())
+      : "";
+    return `${activityString} ${problemString}`;
+  }
 
-	private makeFileInfo(document: TextDocument) {
-		const linecount = document.lineCount;
-		const currentline =
-			window.activeTextEditor.selection.active.line + 1 > linecount
-				? linecount
-				: window.activeTextEditor.selection.active.line + 1;
-		return this.config
-			.get<string>("fileInfoText")
-			.replace(/{linecount}/g, linecount.toString())
-			.replace(/{currentline}/g, currentline.toString())
-			.replace(/{language}/g, document.languageId);
-	}
+  private makeState({
+    info,
+    initial = false,
+  }: {
+    info?: PathInfo;
+    initial?: boolean;
+  }): string {
+    return this.config
+      .get<string>("workspaceText")
+      .replace(/{workspace}/g, workspace.name ?? "no workspace")
+      .replace(/{folder}/g, initial ? "no folder" : info.folder);
+  }
 
-	public fileSwitch(editor: TextEditor) {
-		if (editor) {
-			this.presence.largeImageKey = resolveIcon(editor.document);
-			this.presence.details = this.makeActivity(
-				activity.viewing,
-				editor.document.fileName
-			);
-			if (this.config.get("showFileInfo"))
-				this.presence.largeImageText = this.makeFileInfo(
-					editor.document
-				);
-		} else {
-			this.presence.largeImageKey = vsicon;
-			this.presence.details = undefined;
-			this.presence.largeImageText = this.config.get(placeholderKey);
-		}
-		this.update();
-	}
+  private makeFileInfo(document: TextDocument) {
+    const linecount = document.lineCount;
+    const currentline =
+      window.activeTextEditor.selection.active.line + 1 > linecount
+        ? linecount
+        : window.activeTextEditor.selection.active.line + 1;
+    return this.config
+      .get<string>("fileInfoText")
+      .replace(/{linecount}/g, linecount.toString())
+      .replace(/{currentline}/g, currentline.toString())
+      .replace(/{language}/g, document.languageId);
+  }
 
-	public fileEdit({ document }: TextDocumentChangeEvent) {
-		if (
-			document.fileName.endsWith(".git") ||
-			document.languageId == "scminput"
-		)
-			return;
+  public fileSwitch(editor: TextEditor) {
+    if (editor) {
+      const info = new PathInfo(editor.document);
+      this.presence.largeImageKey = info.icon;
+      this.presence.details = this.makeDetails(activity.viewing, info);
+      this.presence.state = this.makeState({ info });
+      if (this.config.get("showFileInfo"))
+        this.presence.largeImageText = this.makeFileInfo(editor.document);
+    } else {
+      this.presence.largeImageKey = vsicon;
+      this.presence.details = undefined;
+      this.presence.largeImageText = this.config.get(placeholderKey);
+    }
+    this.update();
+  }
 
-		this.presence.largeImageKey = resolveIcon(document);
-		this.presence.details = this.makeActivity(
-			activity.editing,
-			document.fileName
-		);
-		if (this.config.get("showFileInfo"))
-			this.presence.largeImageText = this.makeFileInfo(document);
-		this.update();
-	}
+  public fileEdit({ document }: TextDocumentChangeEvent) {
+    if (document.fileName.endsWith(".git") || document.languageId == "scminput")
+      return;
 
-	public toggleDebug() {
-		this.debugging = !this.debugging;
-	}
+    const info = new PathInfo(document);
+    this.presence.largeImageKey = info.icon;
+    this.presence.details = this.makeDetails(activity.editing, info);
+    this.presence.state = this.makeState({ info });
+    if (this.config.get("showFileInfo"))
+      this.presence.largeImageText = this.makeFileInfo(document);
+    this.update();
+  }
 
-	public diagnosticsChange() {
-		const diag = languages.getDiagnostics();
-		let counted: number = 0;
-		diag.forEach((i) => {
-			if (i[1])
-				i[1].forEach((i) => {
-					if (
-						i.severity == DiagnosticSeverity.Warning ||
-						i.severity == DiagnosticSeverity.Error
-					)
-						counted++;
-				});
-		});
-		this.problems = counted;
-	}
+  public toggleDebug() {
+    this.debugging = !this.debugging;
+  }
 
-	public idle(value: boolean) {
-		this.presence.smallImageKey = value ? icons.idle : vsicon;
-		this.presence.smallImageText = value
-			? this.config.get("idleText")
-			: this.config.get(placeholderKey);
-		this.update();
-	}
+  public diagnosticsChange() {
+    const diag = languages.getDiagnostics();
+    let counted: number = 0;
+    diag.forEach((i) => {
+      if (i[1])
+        i[1].forEach((i) => {
+          if (
+            i.severity == DiagnosticSeverity.Warning ||
+            i.severity == DiagnosticSeverity.Error
+          )
+            counted++;
+        });
+    });
+    this.problems = counted;
+  }
 
-	private update() {
-		this.client.set(this.presence);
-	}
+  public idle(value: boolean) {
+    this.presence.smallImageKey = value ? icons.idle : vsicon;
+    this.presence.smallImageText = value
+      ? this.config.get("idleText")
+      : this.config.get(placeholderKey);
+    this.update();
+  }
 
-	private debugging = false;
+  private update() {
+    this.client.set(this.presence);
+  }
 
-	private problems = 0;
+  private debugging = false;
 
-	private presence: Presence = {};
+  private problems = 0;
 
-	public config: WorkspaceConfiguration;
+  private presence: Presence = {};
+
+  public config: WorkspaceConfiguration;
 }
--- src/util.ts
+++ src/util.ts
@@ -1,22 +1,56 @@
 import type { TextDocument } from "vscode";
 import icons from "./icons.json";
 
-export function resolveIcon(document: TextDocument): string {
-	let icon = icons.find((i) =>
-		i.matches.includes(resolveFileName(document.fileName))
-	);
-	if (!icon)
-		icon = icons.find((i) =>
-			i.matches.includes(resolveFileExtension(document.fileName))
-		);
-	if (!icon)
-		icon = icons.find((i) => i.matches.includes(document.languageId));
+export class PathInfo {
+  constructor(private doc: TextDocument) {}
 
-	return icon ? icon.key : "text";
+  private setFolderAndFileName() {
+    const segments = this.doc.fileName.split(/\/+|\\+/).filter((v) => v);
+    this._fileName = segments.pop();
+    this._folder = segments.pop() ?? "/";
+  }
+
+  private _fileName?: string;
+  public get fileName() {
+    if (!this._fileName) {
+      this.setFolderAndFileName();
+    }
+    return this._fileName;
+  }
+
+  private _folder?: string;
+  public get folder() {
+    if (!this._folder) {
+      this.setFolderAndFileName();
+    }
+    return this._folder;
+  }
+
+  private _extension?: string;
+  public get extension() {
+    if (!this._extension) {
+      if (this._fileName) this._extension = this._fileName.split(".").pop();
+      else {
+        this._extension = this.doc.fileName.split(".").pop();
+      }
+    }
+    return this._extension;
+  }
+
+  private _icon?: string;
+  public get icon() {
+    if (!this._icon) {
+      let icon = icons.find((i) =>
+        i.matches.includes(this._fileName ?? this.fileName)
+      );
+      if (!icon)
+        icon = icons.find((i) =>
+          i.matches.includes(this._extension ?? this.extension)
+        );
+      if (!icon)
+        icon = icons.find((i) => i.matches.includes(this.doc.languageId));
+      this._icon = icon ? icon.key : "text";
+    }
+    return this._icon;
+  }
 }
-
-export const resolveFileName = (file: string): string =>
-	file.split(/(\/)+|(\\)+/).pop();
-
-export const resolveFileExtension = (file: string): string =>
-	file.split(".").pop();
